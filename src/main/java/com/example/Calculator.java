68e6c91e4b62f7742355dd2cf9ebbd02d17dbe44
AnthonyRMatter <134010366+AnthonyRMatter@users.noreply.github.com>
2023-09-01 17:37:46 +0000

    third

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -1,5 +1,5 @@
 package com.example;
 
-public interface Calculator {
-  int calculate(int x, int y);
-}
+// public interface Calculator { // AFTER USING IntBinaryOperator, WE DO NOT NEED THIS INTERFACE ANYMORE
+//   int calculate(int x, int y);
+// }
